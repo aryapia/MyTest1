a78218a3578938df6b6f0ac301b400ef68e7aa52
aryapia <71299760+aryapia@users.noreply.github.com>
2022-01-21 08:19:10 +0800

    Update DemoApplication.java

--- demo/src/main/java/com/pramod/demo/DemoApplication.java
+++ demo/src/main/java/com/pramod/demo/DemoApplication.java
@@ -9,6 +9,7 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 		System.out.println("I am going make this build fail..lets see if Jenkin can catch this);
+				   ARandomClass  myclass = new ARandomClass();
 	}
 
 }
